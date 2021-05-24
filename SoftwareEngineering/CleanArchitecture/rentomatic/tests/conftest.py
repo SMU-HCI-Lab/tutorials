ba5f12f127f9155ff4b7861d7e4398b15700006c
Kotaro Hara <koe.bluebear@gmail.com>
2021-05-25 01:33:29 +0800

    update

--- SoftwareEngineering/CleanArchitecture/rentomatic/tests/conftest.py
+++ SoftwareEngineering/CleanArchitecture/rentomatic/tests/conftest.py
@@ -1,4 +1,7 @@
 import pytest
+import os
+import tempfile
+import yaml
 
 from rentomatic.app import create_app
 from rentomatic.flask_settings import TestConfig
@@ -16,3 +19,46 @@ def pytest_addoption(parser):
 def pytest_runtest_setup(item):
     if 'integration' in item.keywords and not item.config.getvalue("integration"):
         pytest.skip("need --integration option ot run")
+
+
+@pytest.fixture(scope='session')
+def docker_setup(docker_ip):
+    return {
+        'postgres': {
+            'dbname': 'rentomaticsdb',
+            'user': 'postgres',
+            'password': 'rentomaticdb',
+            'host': docker_ip
+        }
+    }
+
+
+@pytest.fixture(scope='session')
+def docker_tmpfile():
+    f = tempfile.mkstemp()
+    yield f
+    os.remove(f[1])
+
+
+@pytest.fixture(scope='session')
+def docker_comp0ose_file(docker_tmpfile, docker_setup):
+    content = {
+        'version': '3.1',
+        'services': {
+            'postgres': {
+                'restart': 'always',
+                'image': 'postgres',
+                'ports': ["5432:5432"],
+                'environment': [
+                    'POSTGRES_PASSWORD={}'.format(
+                        docker_setup['postgres']['password']
+                    )
+                ]
+            }
+        }
+    }
+    f = os.fdopen(docker_tmpfile[0], 'w')
+    f.write(yaml.dump(content))
+    f.close()
+
+    return docker_tmpfile[1]
